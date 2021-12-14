05d48716c30cc89f4ef151db1d840ea2c5649d72
mathiasmichaelweiss <mathiasmichaelweiss@gmail.com>
2021-12-14 10:16:38 +0100

    added bg-profile and user-image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfilePageComponent } from './views/profile-page/profile-page.component';
+import { ProfilePageComponent } from './views/profile-page/profile-page/profile-page.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProfilePageComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProfilePageComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
